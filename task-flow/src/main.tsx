3244a057f23d44060cf71d620fdc8ae0b61a5a7f
sada0918 <shota.0918.fw@gmail.com>
2025-12-14 14:13:44 +0900

    change: プロジェクトをアプリに即した名前に変更

--- task-flow/src/main.tsx
+++ task-flow/src/main.tsx
@@ -2,14 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./feature/home/Home";
-import Articles from "./feature/article/Articles";
+// import Articles from "./feature/article/Articles";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<Home />} />
-        <Route path="/articles" element={<Articles />} />
+        {/* <Route path="/articles" element={<Articles />} /> */}
       </Routes>
     </BrowserRouter>
   </StrictMode>
