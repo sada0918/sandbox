7d6662a505c704fa5e00d4a238d18e09c3010895
sada0918 <shota.0918.fw@gmail.com>
2025-12-14 13:56:13 +0900

    setup: Routing基盤追加

--- bun-vite-react-sample/src/main.tsx
+++ bun-vite-react-sample/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./index.css";
+import Home from "./feature/home/Home";
+import Articles from "./feature/article/Articles";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+        <Route path="/articles" element={<Articles />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
