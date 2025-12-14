31b82848c487e4a2dafc8d7855fe85d839a2eb9b
sada0918 <shota.0918.fw@gmail.com>
2025-12-14 14:03:06 +0900

    delete: デフォルトのCSSファイルを削除

--- bun-vite-react-sample/src/main.tsx
+++ bun-vite-react-sample/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
-import "./index.css";
 import Home from "./feature/home/Home";
 import Articles from "./feature/article/Articles";
 
