9981c000f3118b902273b04689bf45171b3e1c0f
sada0918 <shota.0918.fw@gmail.com>
2025-12-14 15:12:04 +0900

    delete: 不要なコンポーネントを削除

--- task-flow/src/main.tsx
+++ task-flow/src/main.tsx
@@ -1,15 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
-import Home from "./feature/home/Home";
-// import Articles from "./feature/article/Articles";
+import Home from "./features/home/Home";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<Home />} />
-        {/* <Route path="/articles" element={<Articles />} /> */}
       </Routes>
     </BrowserRouter>
   </StrictMode>
